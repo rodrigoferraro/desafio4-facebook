46846f1d76a71c25acbb8c49b2a214f330614607
Rodrigo Ferraro <ferraro.rodrigo@gmail.com>
2020-02-12 17:20:11 -0300

    Title component

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+function Title() {
+  return (
+    <header>
+      <h2>facebook</h2>
+      <h4>Meu perfil</h4>
+    </header>
+  );
+}
+
+export default Title
