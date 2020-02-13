49e6546e0496af21bcb2b1b94487e9f203fe1d37
Rodrigo Ferraro <ferraro.rodrigo@gmail.com>
2020-02-13 11:56:31 -0300

    App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+//import profile from "./assets/avatar.png";
+import Header from "./components/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
